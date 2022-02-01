f10d0375b0e36addd0cf867d0436876661ae8791
Benjamin Auzanneau <benjauzanneau@gmail.com>
2022-02-01 10:37:07 -0600

    added CI

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+install:
+	pip install -e .
+
 test:
 	@coverage run -m pytest tests/*.py
 	@coverage report -m --omit=$(VIRTUAL_ENV)/lib/python*
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
 
+from setuptools import setup
+from setuptools import find_packages
+
 # list dependencies from file
 with open('requirements.txt') as f:
     content = f.readlines()
-requirements = [x.strip() for x in content]
+    requirements = [x.strip() for x in content]
 
-setup(name='taxi_package', # To find the package name within the folder
-      description="functions for taxi project", # Whatever you want
-      packages=find_packages(),
-      install_requires=requirements) # To list the necessary libraries
-#      scripts=['scripts/calculate-distance']) # If you want to install any python script
+setup(
+    name='taxi_package',  # To find the package name within the folder
+    description="functions for taxi project",  # Whatever you want
+    packages=find_packages(),
+    install_requires=requirements,  # To list the necessary libraries
+    scripts=['scripts/me-2-manhattan'
+             ])  # If you want to install any python script
